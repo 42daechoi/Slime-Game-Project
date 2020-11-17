3c7f772e1bf935d585a49d5f6fbe4aa902405463
Cdaey <62363097+Cdaey@users.noreply.github.com>
2020-11-18 02:17:56 +0900

    원소 상호작용, 데미지 매커니즘 추가

--- Assets/Script/PlayerControler.cs
+++ Assets/Script/PlayerControler.cs
@@ -11,8 +11,14 @@ public class PlayerControler : MonoBehaviour
     [SerializeField]        
     Transform slimeBody;    //슬라임 트랜스폼 
 
+    GameObject nearObj;     //가까이 있는 오브젝트를 저장할 변수   //내가 가지고 있는 원소
+
     public float spd=5f;    //캐릭터 걷기 이동속도
     public float jumppw=10f;//점프 파워
+    public int damage=20;
+    public int elementCnt=0;    //엘리먼트 강화 숫자
+    public int elementIndex=-2; //불:0, 얼음:1, 물:2, 돌:3, 풀:4, 빛:5의 인덱스를 가짐 -2는 초기화
+    public int myElement=-1;    //내가 가진 원소의 Index, -1은 초기화
 
     float hAxis;            //키보드 a,d 또는 방향키 양옆 입력 받을 변수
     float vAxis;            //키보드 w,s 또는 방향키 앞뒤 입력 받을 변수
@@ -28,6 +34,7 @@ public class PlayerControler : MonoBehaviour
     bool Altflag;
     bool isJump;
     bool isDodge;
+    bool Eflag;
 
 
     void Awake()
@@ -38,12 +45,13 @@ public class PlayerControler : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {   
         GetInput();
         LookAround();
         Move();
         Run();
         Jump();
+        Interaction();
     }
     
     
@@ -57,12 +65,12 @@ public class PlayerControler : MonoBehaviour
         runflag=Input.GetKey(KeyCode.LeftShift);
         jumpflag=Input.GetButtonDown("Jump");
         Altflag=Input.GetKey(KeyCode.LeftAlt);
-
+        Eflag=Input.GetKeyDown(KeyCode.E);
         //입력받는 함수
     }
 
     void LookAround()
-    {
+    {   
         Vector2 mouseDelta = new Vector2(xMouse,yMouse);        //이동 전후의 마우스 값 차이
         Vector3 camAngle = cameraArm.rotation.eulerAngles;      //카메라 회전을 오일러 값으로 변환
         float xLimit = camAngle.x - mouseDelta.y;               //71번 라인 x좌표에 들어가기 전에 조건문을 위해 변수 선언 후 미리 계산
@@ -116,6 +124,29 @@ public class PlayerControler : MonoBehaviour
             isJump=true;                    //점프 애니메이션 실행이 아닌 코드 내부 점프 여부 확인 flag
         }
     }
+
+    void Interaction()
+    {   
+        if(Eflag && nearObj != null && !isJump && nearObj.tag == "Tag_Element" && elementCnt<5)
+        {   
+            Elements el = nearObj.GetComponent<Elements>();
+            elementIndex=el.value;
+            if(myElement==-1){  //원소를 가지고 있지 않은 경우
+                elementCnt++;
+                myElement=elementIndex;
+            }
+            else if(myElement==elementIndex) {  //내가 먹은 원소가 내가 가지고 있던 원소와 같은 경우
+                damage+=10;
+                elementCnt++;
+            }
+            else {  //내가 가지고 있던 원소랑 내가 먹은 원소가 다른경우
+                damage=20;
+                elementCnt=1;
+                myElement=elementIndex;
+            }
+            Destroy(nearObj); //상호작용 시 파괴
+        }
+    }
     
     void OnCollisionEnter(Collision collision)
     {
@@ -125,4 +156,16 @@ public class PlayerControler : MonoBehaviour
         }
     }
 
+    void OnTriggerStay(Collider other) //트리거(콜라이더 범위)에 닿여있을 때 실행하는 함수
+    {
+        if(other.tag == "Tag_Element") nearObj = other.gameObject; //원소 트리거 발생 시 nearObj에 부딛힌 오브젝트를 저장
+        //Sphere Collider 에 isTrigger 체크 시에 충돌체가 아닌 트리거가 된다.
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Tag_Element") nearObj = null;
+        //콜라이더 범위 탈출 시 nearObj 초기화
+    }
+    
 }
